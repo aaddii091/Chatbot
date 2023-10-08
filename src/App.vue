2b64ac5334ab8ee2c26343991bfe691f545753c8
Aditya Saxena <aaddii091@gmail.com>
2023-10-08 18:37:09 +0530

    Starting Chatbot

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import dashboardView from "./components/dashboardView.vue";
+</script>
+
+<template>
+  <div>
+    <dashboard-view />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
