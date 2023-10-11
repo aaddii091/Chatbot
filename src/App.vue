1cea344a325fad2f144f1d1e2b28ad63b47b379e
Tejas013 <tejasmadhukar6@gmail.com>
2023-10-12 02:41:47 +0530

    moved main chat and added dyanmic nav

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
-<script setup>
-import dashboardView from "./components/dashboardView.vue";
-import navbarView from "./components/navbarView.vue";
-console.log(import.meta.env.VITE_OPENAI_API_KEY);
-</script>
-
 <template>
-  <div class="display">
-    <navbar-view class="navbar" />
-    <dashboard-view class="dashboard" />
+  <div>
+    <nav>
+      <router-link to="/" class=" text-slate-50"> Home </router-link> |
+      <span> 
+        <router-link to="/chat" class=" text-slate-50"> Chat Page </router-link> |
+      </span>
+      <span v-if="isLoggedIn"> 
+        <button @click="handleSignOut" class="text-slate-50"> Logout </button> 
+      </span>
+      <span v-else>
+        <router-link to="/signup" class=" text-slate-50"> Signup </router-link> |
+      </span>
+      
+    </nav>
+    <router-view />
   </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref, watchEffect } from 'vue' // used for conditional rendering
+import { getAuth,onAuthStateChanged, signOut } from 'firebase/auth'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const isLoggedIn = ref(true)
+
+// runs after firebase is initialized
+onAuthStateChanged(getAuth(),function(user) {
+    if (user) {
+      isLoggedIn.value = true // if we have a user
+    } else {
+      isLoggedIn.value = false // if we do not
+    }
+})
+
+const handleSignOut = () => {
+  signOut(getAuth())
+  router.push('/')
+}
+</script>
+
+<style scoped>
+
+</style>
